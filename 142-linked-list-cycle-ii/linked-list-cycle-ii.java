dee7b0f158ad484f0113dfe910b8b5345622e2ea
Avishkar Hongekar <69665805+ItsAvi165@users.noreply.github.com>
2024-03-01 21:41:20 +0530

    Time: 1 ms (35.08%) | Memory: 44.2 MB (90.18%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,36 +1,37 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return null;
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return null;
         ListNode slow = head;
         ListNode fast = head;
         boolean exit = false;
-        while(exit!=true){
-            if(fast.next !=null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            }
-            else{
+        while (exit != true) {
+            if (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            } else {
                 return null;
             }
-           // if(fast == null) return null;
-            if(slow == fast){
+            // if(fast == null) return null;
+            if (slow == fast) {
                 exit = true;
             }
-        } 
+        }
         fast = head;
-        while(fast!=slow){
+        while (fast != slow) {
             slow = slow.next;
             fast = fast.next;
         }
