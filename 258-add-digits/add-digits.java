2c891de4e26ef2d525332fb851ab1d8653c7fe71
Avishkar Hongekar <69665805+ItsAvi165@users.noreply.github.com>
2024-03-23 12:01:59 +0530

    Time: 1 ms (48.05%) | Memory: 40.6 MB (81.74%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -4,8 +4,8 @@ class Solution {
         while(num >= 10){
                 sum = 0;
             while(num>0){
-                sum = sum + (num%10);
-                num = num / 10;
+                sum += (num%10);
+                num /= 10;
             }
             num = sum;
         }
