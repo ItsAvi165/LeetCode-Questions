8826df49e94ba2415251fd97033bcad672795dca
Avishkar Hongekar <69665805+ItsAvi165@users.noreply.github.com>
2026-01-18 22:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (55.34%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        List<Integer> al = Arrays.stream(nums).boxed().collect(Collectors.toList());
         int n = nums.length;
-        int ans = 0;
-        for(int i=0;i<=n;i++){
-            if(!al.contains(i)) ans = i;
-        }
-        return ans;
+        int sumOfNNumbers = n*(n+1)/2;
+        int sumOfArray = 0;
 
+        for(int i=0;i<n;i++){
+            sumOfArray += nums[i];
+        }
+
+        return sumOfNNumbers - sumOfArray;
     }
 }
